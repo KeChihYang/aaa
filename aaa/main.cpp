d97562fcfaa9eff04cb0f21bfd4ba93d861efa5e
steve <steve@steves-MacBook-Air.local>
2014-05-28 18:21:02 +0800

    Initial Commit

---
+++ aaa/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  aaa
+//
+//  Created by steve on 2014/5/28.
+//  Copyright (c) 2014年 steve. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
